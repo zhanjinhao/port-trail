0120d788b80172bde95168a9dd19b45d6b751fca
addenda <isjinhao@163.com>
2026-07-12 11:49:48 +0800

    fix: TaskExecuteInterceptorPoint#getMethodsMatcher

--- port-trail-agent/src/main/java/cn/addenda/porttrail/agent/transform/interceptor/task/TaskExecuteInterceptorPoint.java
+++ port-trail-agent/src/main/java/cn/addenda/porttrail/agent/transform/interceptor/task/TaskExecuteInterceptorPoint.java
@@ -16,21 +16,20 @@ public class TaskExecuteInterceptorPoint implements InterceptorPoint {
             .and(ElementMatchers.isDeclaredBy(java.util.concurrent.Callable.class))
             .or(ElementMatchers.named("accept")
                     .and(ElementMatchers.isPublic())
-                    .and(ElementMatchers.takesArguments(1)))
-            .and(ElementMatchers.isDeclaredBy(java.util.function.Consumer.class))
+                    .and(ElementMatchers.takesArguments(1))
+                    .and(ElementMatchers.isDeclaredBy(java.util.function.Consumer.class)))
             .or(ElementMatchers.named("apply")
                     .and(ElementMatchers.isPublic())
-                    .and(ElementMatchers.takesArguments(1)))
-            .and(ElementMatchers.isDeclaredBy(java.util.function.Function.class))
+                    .and(ElementMatchers.takesArguments(1))
+                    .and(ElementMatchers.isDeclaredBy(java.util.function.Function.class)))
             .or(ElementMatchers.named("run")
                     .and(ElementMatchers.isPublic())
-                    .and(ElementMatchers.takesArguments(0)))
-            .and(ElementMatchers.isDeclaredBy(java.lang.Runnable.class))
+                    .and(ElementMatchers.takesArguments(0))
+                    .and(ElementMatchers.isDeclaredBy(java.lang.Runnable.class)))
             .or(ElementMatchers.named("get")
                     .and(ElementMatchers.isPublic())
                     .and(ElementMatchers.takesArguments(0))
-                    .and(ElementMatchers.isDeclaredBy(java.util.function.Supplier.class))
-            );
+                    .and(ElementMatchers.isDeclaredBy(java.util.function.Supplier.class)));
   }
 
   @Override
